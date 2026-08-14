f90160c3793bce4b12e765f0d278d92d78e1e508
JorgeDoicela <ismael02doicela@gmail.com>
2026-08-14 16:14:16 -0500

    feat: añadir variables de sesión globales de Wayland, nixfmt y .gitignore profesional

--- home.nix
+++ home.nix
@@ -8,6 +8,18 @@
   # Habilitar la gestión propia de Home Manager
   programs.home-manager.enable = true;
 
+  # Variables de entorno globales del sistema de usuario (Wayland / Qt / Electron)
+  home.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+  };
+
   # Importar módulos modulares de configuración
   imports = [
     ./modules/style.nix
@@ -24,5 +36,6 @@
     bibata-cursors
     font-awesome
     nerd-fonts.jetbrains-mono
+    nixfmt-rfc-style # Formateador oficial de código Nix
   ];
 }
