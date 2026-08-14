f7f5d8310431ab48eb144957cf4988a1fbfb27e2
JorgeDoicela <ismael02doicela@gmail.com>
2026-08-14 17:37:13 -0500

    fix: configurar respuesta instantanea (-hd 0) para despliegue por hover en el Dock

--- raw_configs/hypr/start_nwg_dock.sh
+++ raw_configs/hypr/start_nwg_dock.sh
@@ -7,7 +7,7 @@ killall nwg-dock-hyprland 2>/dev/null
 sleep 0.2
 
 # Iniciar directamente en segundo plano y desacoplar del proceso padre
-nwg-dock-hyprland -i 40 -d -mb 8 -nolauncher >/dev/null 2>&1 &
+nwg-dock-hyprland -i 40 -d -hd 0 -mb 6 -nolauncher >/dev/null 2>&1 &
 disown
 
 echo "[Dock] nwg-dock-hyprland iniciado de forma autónoma."
